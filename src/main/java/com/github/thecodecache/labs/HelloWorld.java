3ee14ddc27b0ade0daba6d4ebf2bc8c861c19a90
manoranjan <softmanoranjan@gmail.com>
2021-10-17 02:10:08 +0530

    initial package structure

---
+++ src/main/java/com/github/thecodecache/labs/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.github.thecodecache.labs;
+
+/**
+ * @author Hp
+ */
+public class HelloWorld {
+
+	/**
+	 * Entry Point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello - The Code Cache!");
+	}
+}
